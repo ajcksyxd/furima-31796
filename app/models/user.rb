b60dda20e162b9f81e287f9060c10defe114ff58
ajcksyxd <a2413414649@gmail.com>
2020-11-15 14:45:18 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
   NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z+\d]+\z/i.freeze
-  
+
   with_options presence: true do
     validates :nickname
     validates :last_name, format: { with: NAME_REGEX, message: 'is invalid. Input full-width characters.' }
