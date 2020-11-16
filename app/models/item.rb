e823aa8a4882500045dd7ee556d153fe0746dfc5
ajcksyxd <a2413414649@gmail.com>
2020-11-16 19:14:21 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days_to_ship
 
-  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, numericality: { only_integer: greater_than_equual_to: 300, less_than_or_equal_to: 999999999 }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :days_to_ship_id
 end
