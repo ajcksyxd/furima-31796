a620a37cf5946e98bf86a9199264f25fead96eb7
ajcksyxd <a2413414649@gmail.com>
2020-11-17 11:21:11 +0900

    出品した商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
   end
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
 
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 0, message: "status Select" } do
     validates :category_id
     validates :condition_id
     validates :shipping_charge_id
