ef567156ff3e109c43a098c493e8a8bc36f6eaa4
ajcksyxd <a2413414649@gmail.com>
2020-11-19 21:20:56 +0900

    ビューファイルの配置

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,5 +1,5 @@
 class PurchaseAddress
-  include ActiveModel: :Model
+  include ActiveModel::Model
   attr_accessor :postal_code, :prefecture, :municipality, :house_namber, :building_name, :phone_number
 
   POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}+\z/.freeze
@@ -14,7 +14,7 @@ class PurchaseAddress
   end
 
   def save
-    user = User.create(nickname: nickname, :email email, encrypted_password: encrypted_password, last_name: last_name, first_name: first_name, last_name_kana: last_name_kana, first_name_kana: first_name_kana, birthday: birthday)
+    user = User.create(nickname: nickname, email: email, encrypted_password: encrypted_password, last_name: last_name, first_name: first_name, last_name_kana: last_name_kana, first_name_kana: first_name_kana, birthday: birthday)
     item = Item.create(name: name, description: description, category: category, condition: condition, shipping_charge: shipping_charge, prefecture: prefecture, days_to_ship: days_to_ship, price: price)
     purchase = Purchase.create(user_id: user.id, item_id: item.id)
     Address.create(postal_code: postal_code, prefecture: prefecture, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
